f9cad778c47ed6fa6b01c220cc6230665657195e
yucl80 <147286318@qq. com>
2022-04-25 16:11:11 +0800

    init

--- src/main/java/com/yucl/learn/async/Testkotlin.java
+++ src/main/java/com/yucl/learn/async/Testkotlin.java
@@ -4,6 +4,19 @@ import kotlin.Unit;
 import kotlin.coroutines.Continuation;
 import kotlin.jvm.functions.Function2;
 import kotlinx.coroutines.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
 
 public class Testkotlin {
     public static void main(String[] args) {
@@ -13,19 +26,27 @@ public class Testkotlin {
                 CoroutineStart.DEFAULT, // CoroutineStart.LAZY, or other strategies
                (coroutineScope, continuation) -> {
                     // do your stuff
-                    return "Some result";
-                }
+                   return httpGet();
+               }
         );
+        System.out.println("hello");
+
+
 
         Job job = BuildersKt.launch(
                 GlobalScope.INSTANCE,
                 GlobalScope.INSTANCE.getCoroutineContext(),
                 CoroutineStart.DEFAULT, // CoroutineStart.LAZY, or other strategies
                 (Function2<CoroutineScope, Continuation<? super Unit>, Unit>) (coroutineScope, continuation) -> {
-                    // do your stuff
+                   String data =  httpGet();
+                   System.out.println(data);
                     return Unit.INSTANCE;
                 }
         );
+
+
+
+
 
         try {
             // Usually used to bridge regular blocking code to libraries using suspend,
@@ -34,7 +55,7 @@ public class Testkotlin {
                     GlobalScope.INSTANCE.getCoroutineContext(),
                     (Function2<CoroutineScope, Continuation<? super String>, String>) (coroutineScope, continuation) -> {
                         // do your stuff
-                        return "Some result";
+                        return httpGet();
                     }
             );
             System.out.println(result);
@@ -44,5 +65,23 @@ public class Testkotlin {
             // *
             // Do something with the interruption error
         }
+    }
+
+    private static String httpGet() {
+        try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            final HttpGet httpget = new HttpGet("http://httpbin.org/get");
+            System.out.println("Executing request " + httpget.getMethod() + " " + httpget.getURI());
+
+            final String responseBody = httpclient.execute(httpget, new ResponseHandler<String>() {
+                @Override
+                public String handleResponse(HttpResponse httpResponse) throws ClientProtocolException, IOException {
+                    return EntityUtils.toString(httpResponse.getEntity());
+                }
+            });
+            return responseBody;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "Some result";
     }
 }
